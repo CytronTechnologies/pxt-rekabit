1d555defabe87a98e6b35e768fe6357be0fbc632
Kong Wai Weng <waiweng@cytron.com.my>
2021-01-15 18:11:17 +0800

    Added RGB

--- rgb.ts
+++ rgb.ts
@@ -1,1 +1,172 @@
-// Add your code here
+/*******************************************************************************
+ * Functions for REKA:BIT - RGB Bit.
+ *
+ * Company: Cytron Technologies Sdn Bhd
+ * Website: http://www.cytron.io
+ * Email:   support@cytron.io
+ *******************************************************************************/
+
+// Default pin.
+const RGB_LED_PIN = DigitalPin.P8;
+
+// Number of RGB LED.
+const RGB_LED_LENGTH = 2;
+
+// RGB Colors
+enum RgbColors {
+    //% block=red
+    Red = 0xFF0000,
+    //% block=orange
+    Orange = 0xFFA500,
+    //% block=yellow
+    Yellow = 0xFFFF00,
+    //% block=green
+    Green = 0x00FF00,
+    //% block=blue
+    Blue = 0x0000FF,
+    //% block=indigo
+    Indigo = 0x4b0082,
+    //% block=violet
+    Violet = 0x8a2be2,
+    //% block=purple
+    Purple = 0xFF00FF,
+    //% block=white
+    White = 0xFFFFFF,
+    //% block=black
+    Black = 0x000000
+}
+
+
+
+/**
+ * Blocks for RGB LED.
+ */
+//% weight=12 color=#ff8000 icon="\uf110" block="RGB Bit"
+namespace rekabitRgbLed {
+    // Colors array for each pixel.
+    let colorsArray: number[] = [];
+    for (let i = 0; i < RGB_LED_LENGTH; i++) {
+        colorsArray.push(0);
+    }
+
+    // Create a Neo Pixel object for RGB LED.
+    let rgbLed = neopixel.create(RGB_LED_PIN, RGB_LED_LENGTH, NeoPixelMode.RGB);
+    rgbLed.clear();
+
+    // Reduce the default brightness.
+    rgbLed.setBrightness(25);
+
+
+
+    /**
+     * Turn off all RGB pixels.
+     */
+    //% weight=20
+    //% blockGap=8
+    //% blockId="rekabit_clear_pixels"
+    //% block="clear all RGB pixels"
+    export function clear(): void {
+        for (let i = 0; i < RGB_LED_LENGTH; i++) {
+            colorsArray[i] = 0;
+        }
+
+        rgbLed.clear();
+        rgbLed.show();
+        basic.pause(0);
+    }
+
+
+    /**
+     * Set the brightness of the RGB pixels (0-255).
+     * @param brightness Pixel brightness. eg: 25
+     */
+    //% weight=19
+    //% blockGap=40
+    //% blockId="rekabit_set_brightness"
+    //% block="set RGB pixels brightness to %brightness"
+    //% brightness.min=0 brightness.max=255
+    export function setBrightness(brightness: number): void {
+        rgbLed.setBrightness(brightness);
+
+        // Restore the original color.
+        for (let i = 0; i < RGB_LED_LENGTH; i++) {
+            rgbLed.setPixelColor(i, colorsArray[i]);
+        }
+        rgbLed.show();
+        basic.pause(0);
+    }
+
+
+    /**
+     * Show the same color on all RGB pixels. 
+     * @param color RGB color of the pixel.
+     */
+    //% weight=17
+    //% blockGap=8
+    //% blockId="rekabit_show_color"
+    //% block="set all RGB pixels to %color"
+    //% color.shadow="colorNumberPicker"
+    export function showColor(color: number): void {
+        for (let i = 0; i < RGB_LED_LENGTH; i++) {
+            colorsArray[i] = color;
+        }
+        rgbLed.showColor(color);
+        basic.pause(0);
+    }
+
+
+    /**
+     * Show color on individual RGB pixel.
+     * @param pixel The pixel number we want to change the color.
+     * @param color RGB color of the pixel.
+     */
+    //% weight=16
+    //% blockGap=40
+    //% blockId="rekabit_set_pixel_color"
+    //% block="set RGB pixel %pixel to %color"
+    //% color.shadow="colorNumberPicker"
+    //% pixel.min=0 pixel.max=1
+    export function setPixelColor(pixel: number, color: number): void {
+        colorsArray[pixel] = color;
+        rgbLed.setPixelColor(pixel, color);
+        rgbLed.show();
+        basic.pause(0);
+    }
+
+
+
+    /**
+     * Return the RGB value of a known color.
+    */
+    //% weight=13
+    //% blockGap=8
+    //% blockId="rekabit_colors"
+    //% block="%color"
+    export function colors(color: RgbColors): number {
+        return <number>color;
+    }
+
+
+    /**
+     * Converts red, green, blue channels into a RGB color.
+     * @param red Value of the red channel (0 - 255). eg: 255
+     * @param green Value of the green channel (0 - 255). eg: 255
+     * @param blue Value of the blue channel (0 - 255). eg: 255
+     */
+    //% weight=12
+    //% blockGap=30
+    //% blockId="rekabit_rgb_value"
+    //% block="red %red green %green blue %blue"
+    //% red.min=0 red.max=255
+    //% green.min=0 green.max=255
+    //% blue.min=0 blue.max=255
+    export function rgb(red: number, green: number, blue: number): number {
+        // Limit the value.
+        red = rekabit.limit(red, 0, 255);
+        green = rekabit.limit(green, 0, 255);
+        blue = rekabit.limit(blue, 0, 255);
+
+        return ((red & 0xFF) << 16) | ((green & 0xFF) << 8) | (blue & 0xFF);
+    }
+
+}
--- test.ts
+++ test.ts
@@ -1,1 +1,100 @@
-// tests go here; this will not be compiled when this package is used as an extension.
+// tests go here; this will not be compiled when this package is used as a library
+
+// Loop forever.
+basic.forever(function () {
+    // Plot the power input voltage on 4th row LED 1.
+    if (rekabitPower.readVin() > 4.0) {
+        led.plot(0, 3);
+    } else {
+        led.unplot(0, 3);
+    }
+
+    // Plot the power state on 4th row LED 2.
+    if (rekabitPower.isPowerOn()) {
+        led.plot(1, 3);
+    } else {
+        led.unplot(1, 3);
+    }
+
+    // Plot the low batt state on 4th row LED 3.
+    if (rekabitPower.isLowBatt()) {
+        led.plot(2, 3);
+    } else {
+        led.unplot(2, 3);
+    }
+
+    // Plot the overvoltage state on 4th row LED 4.
+    if (rekabitPower.isOvervoltage()) {
+        led.plot(3, 3);
+    } else {
+        led.unplot(3, 3);
+    }
+
+
+
+    // Show red color on all RGB pixels.
+    rekabitRgbLed.showColor(0xff0000)
+    basic.pause(1000)
+
+    // Show different color on each RGB pixels.
+    rekabitRgbLed.setPixelColor(0, 0xff0000)
+    rekabitRgbLed.setPixelColor(1, 0x00ff00)
+    basic.pause(1000)
+
+    // Change the brightness to 100% and show rainbow color.
+    rekabitRgbLed.setBrightness(255)
+    basic.pause(1000)
+
+    // Clear all RGB pixels.
+    rekabitRgbLed.clear()
+
+
+
+    // Run Motor 1 forward at 50% speed for 1 second.
+    rekabitMotors.runMotor(MotorChannel.M1, MotorDirection.Forward, 127)
+    basic.pause(1000)
+    rekabitMotors.brakeMotor(MotorChannel.M1)
+
+    // Run Motor 2 backward at 100% speed for 1 second.
+    rekabitMotors.runMotor(MotorChannel.M2, MotorDirection.Backward, 255)
+    basic.pause(1000)
+    rekabitMotors.brakeMotor(MotorChannel.M2)
+
+
+
+    // Move Servo 1 to 0 degree.
+    rekabitMotors.setServoPosition(ServoChannel.S1, 0)
+    basic.pause(1000)
+
+    // Disable Servo 1.
+    rekabitMotors.disableServo(ServoChannel.S1)
+
+
+
+    // Move Servo 2 to 180 degrees.
+    rekabitMotors.setServoPosition(ServoChannel.S2, 180)
+    basic.pause(1000)
+
+    // Disable Servo 2.
+    rekabitMotors.disableServo(ServoChannel.S2)
+
+
+
+    // Move Servo 3 to 0 degree.
+    rekabitMotors.setServoPosition(ServoChannel.S3, 0)
+    basic.pause(1000)
+
+    // Disable Servo 3.
+    basic.pause(1000)
+    rekabitMotors.disableServo(ServoChannel.S3)
+	
+	
+	
+	// Move Servo 4 to 180 degree.
+    rekabitMotors.setServoPosition(ServoChannel.S4, 180)
+    basic.pause(1000)
+
+    // Disable Servo 4.
+    basic.pause(1000)
+    rekabitMotors.disableServo(ServoChannel.S4)
+})
